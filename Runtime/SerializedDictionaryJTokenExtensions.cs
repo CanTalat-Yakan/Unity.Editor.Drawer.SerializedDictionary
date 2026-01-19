9e2e0d3ea856d394cefa05385a56e8f8b2fad691
CanTalat-Yakan <can.talat.yakan@gmail.com>
2026-01-19 17:09:29 +0100

    ⠀⠀⠀⠀⠀

--- Runtime/SerializedDictionaryJTokenExtensions.cs
+++ Runtime/SerializedDictionaryJTokenExtensions.cs
@@ -26,11 +26,13 @@ namespace UnityEssentials
         public static void SetInt(this SerializedDictionary<string, JToken> dict, string key, int value) => dict.Set(key, value);
         public static void SetFloat(this SerializedDictionary<string, JToken> dict, string key, float value) => dict.Set(key, value);
         public static void SetBool(this SerializedDictionary<string, JToken> dict, string key, bool value) => dict.Set(key, value);
+        public static void SetColor(this SerializedDictionary<string, JToken> dict, string key, UnityEngine.Color value) => dict.Set(key, value);
 
         public static string GetString(this SerializedDictionary<string, JToken> dict, string key, string defaultValue = "") => dict.Get(key, defaultValue);
         public static int GetInt(this SerializedDictionary<string, JToken> dict, string key, int defaultValue = 0) => dict.Get(key, defaultValue);
         public static float GetFloat(this SerializedDictionary<string, JToken> dict, string key, float defaultValue = 0f) => dict.Get(key, defaultValue);
         public static bool GetBool(this SerializedDictionary<string, JToken> dict, string key, bool defaultValue = false) => dict.Get(key, defaultValue);
+        public static UnityEngine.Color GetColor(this SerializedDictionary<string, JToken> dict, string key, UnityEngine.Color defaultValue) => dict.Get(key, defaultValue);
 
         public static void Set<T>(this SerializedDictionary<string, JToken> dict, string key, T value)
         {
